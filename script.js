ae7ec7a020e64ad7424d4853a1d9fc97dd0cb24d
Isabel Dec <idec@id.iit.edu>
2017-04-07 12:53:41 -0500

    creating many clickable boxes.

--- script.js
+++ script.js
@@ -6,7 +6,9 @@
 // do/while 
 // while 
 
-var boxes = document.querySelectorAll(".box");
+
+
+// var boxes = document.querySelectorAll(".box");
 
 // console.log(boxes);
 
@@ -14,22 +16,22 @@ var boxes = document.querySelectorAll(".box");
 
 // }
 
-if (boxes.length > 4) {
-    console.log("We have more than 4 boxes");
+// if (boxes.length > 4) {
+//     console.log("We have more than 4 boxes");
     
-}
+// }
 
-if (boxe.length <= 4) {
-    console.log("We have less than or equal to 4 boxes");
+// if (boxe.length <= 4) {
+//     console.log("We have less than or equal to 4 boxes");
 
-}
+// }
 
-// same as above essentially 
-if (boxe.length > 4) {
-    console.log("We have more than 4 boxes");
-} else {
-    console.log("We have less than or equal to 4 boxes");
-}
+// // same as above essentially 
+// if (boxe.length > 4) {
+//     console.log("We have more than 4 boxes");
+// } else {
+//     console.log("We have less than or equal to 4 boxes");
+// }
 
 // if (boxes.length > 2) {
 //     //
@@ -39,19 +41,19 @@ if (boxe.length > 4) {
 
 // }
 
-switch (boxes.length) {
-    case 1: 
-        console.log("we have 1box.");
-        break;
+// switch (boxes.length) {
+//     case 1: 
+//         console.log("we have 1box.");
+//         break;
 
-    case 2: 
-        console.log("we have 2boxes.");
-        break;
+//     case 2: 
+//         console.log("we have 2boxes.");
+//         break;
 
-    default:
-        console.log("this is default.");
-        break; 
-}
+//     default:
+//         console.log("this is default.");
+//         break; 
+// }
 
 // // another way of saying the above would be
 // if (boxes.length == 1) {
@@ -59,3 +61,56 @@ switch (boxes.length) {
 // } else if (boxes.lenght == 2) { 
 
 // }
+
+
+// new -> example of a 'string' (or bunch of words in quotes ?)
+var name = "Izzy Dec"; 
+
+console.log(name.length); 
+
+
+// example of 'arrays' work (everything needs to end with a comma!)
+var fruits = [
+    "orange",
+    "durian",
+    "banana",
+    "cantelope",
+    "pinapple", 
+    "pomogranate",
+];
+
+console.log(fruits.length);
+console.log(fruits[2]);
+console.log(fruits[2].length);
+
+
+
+var daysInYear = 365.25;
+
+console.log(daysInYear);
+
+
+if (fruits[2] == "banana") {
+    document.body.style.backgroundColor = "yellow";
+
+}
+
+
+// for (var i = 0; i < 100; i += 1)
+// // same as saying...
+
+// for (var i = 0; i < 100; i++) {
+//     console.log(i);
+// }
+
+
+function boxClicked() {
+    console.log("box clicked");
+}
+
+var boxes = document.querySelectorAll(".box");
+console.log(boxes.length);
+
+for (var i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener("click", boxClicked);
+}
